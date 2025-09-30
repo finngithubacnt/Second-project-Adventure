208892d3c097ac903bcc8590682b0c8ccce02d97
MUKILTEO\1717487 <fgreenmun@gmail.com>
2025-09-30 13:15:38 -0700

    enemys. player updates , darkness and fixes

---
+++ 2D Template/Assets/People/EnemyHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float Ehealth = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Ehealth = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D Template/Assets/People/PlayerHealth.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float Phealth = 10;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Phealth = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
