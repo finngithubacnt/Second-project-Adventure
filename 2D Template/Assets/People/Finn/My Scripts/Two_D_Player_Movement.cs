9523451935a231e1b8ce5183110790f816a917ca
MUKILTEO\1726619 <1726619@mukilteo.wednet.edu>
2025-10-02 13:28:55 -0700

    Added animation to knight

--- 2D Template/Assets/People/Finn/My Scripts/Two_D_Player_Movement.cs
+++ 2D Template/Assets/People/Finn/My Scripts/Two_D_Player_Movement.cs
@@ -5,11 +5,13 @@ public class Two_D_Player_Movement : MonoBehaviour
     Rigidbody2D rb;
     public float speed = 5;
     public float speedLimit = 10;
+    Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,28 +31,45 @@ public class Two_D_Player_Movement : MonoBehaviour
             if (rb.linearVelocityY < speedLimit)
             {
                 rb.AddForceY(speed);
+                animator.SetFloat("VerticalSpeed", speed);
             }
         }
+
         if (Input.GetKey(KeyCode.A))//Left
         {
             if (rb.linearVelocityX > -speedLimit)
             {
                 rb.AddForceX(-speed);
+                animator.SetFloat("HorizontalSpeed", -speed);
             }
         }
+
         if (Input.GetKey(KeyCode.S))//Down
         {
-                if (rb.linearVelocityY > -speedLimit)
-                {
-                    rb.AddForceY(-speed);
-                }
+             if (rb.linearVelocityY > -speedLimit)
+             {
+                rb.AddForceY(-speed);
+                animator.SetFloat("VerticalSpeed", -speed);
+             }
         }
+
         if (Input.GetKey(KeyCode.D))//Right
         {
-                   if (rb.linearVelocityX < speedLimit)
-                    {
-                        rb.AddForceX(speed);
-                    }
+            if (rb.linearVelocityX < speedLimit)
+            {
+                rb.AddForceX(speed);
+                animator.SetFloat("HorizontalSpeed", speed);
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetFloat("VerticalSpeed", 0);
+        }
+        
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetFloat("HorizontalSpeed", 0);
         }
     }
 }
