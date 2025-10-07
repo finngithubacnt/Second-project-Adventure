2a98475702b07d88c695a4e0c781a1ad3eb55448
MUKILTEO\1725476 <1725476@mukilteo.wednet.edu>
2025-10-07 13:26:32 -0700

    thanks

--- 2D Template/Assets/Scenes/palettes/pressure plate.cs
+++ 2D Template/Assets/Scenes/palettes/pressure plate.cs
@@ -1,29 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class pressure_plate : MonoBehaviour
 {
     public GameObject door;
     private Animator anim;
     private bool isOpen = false;
+
+    public UnityEvent onPlatePressed;
+
     void Start()
     {
         anim = door.GetComponent<Animator>();
     }
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !isOpen)
         {
-            anim.SetTrigger("Open");
+            //anim.SetTrigger("Open");
             isOpen = true;
+            onPlatePressed.Invoke();
         }
     }
-    void OnTriggerExit(Collider other)
+
+    void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Player") && isOpen)
         {
-            anim.SetTrigger("Close");
+            //anim.SetTrigger("Close");
             isOpen = false;
         }
     }
